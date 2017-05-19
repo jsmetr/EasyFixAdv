8f6d59f21ce27992b303c3d1517a6fb41ca4cdb5
Jarno <Jarno@JPC>
2017-05-20 02:55:31 +0300

    Added reviews.js

---
+++ src/main/webapp/easyfix-js/reviews.js
@@ -0,0 +1,8 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//in time, this will hold all js used by the review page. For now, just this to push things through travis.
